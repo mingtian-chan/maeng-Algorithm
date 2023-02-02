dfb4845846fa31755780de750a340ca0ec9c0db7
Myoung Chan, Kim <89979849+mingtian-chan@users.noreply.github.com>
2023-02-02 15:18:48 +0900

    [Bronze II] Title: 저항, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/1076. 저항/저항.py
@@ -0,0 +1,6 @@
+
+color = {"black": 0, "brown": 1, "red": 2, "orange": 3, "yellow": 4, "green": 5, "blue": 6, "violet": 7, "grey": 8, "white": 9}
+a = input()
+b = input()
+c = input()
+print((color[a]*10+color[b])*(10**color[c]))
