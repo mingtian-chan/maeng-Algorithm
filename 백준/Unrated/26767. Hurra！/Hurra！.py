84925d55ec645b637afb28f41c730387fb35d517
Myoung Chan, Kim <89979849+mingtian-chan@users.noreply.github.com>
2023-03-18 22:11:23 +0900

    [Unrated] Title: Hurra!, Time: 692 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Unrated/26767. Hurra！/Hurra！.py
@@ -0,0 +1,10 @@
+T = int(input())
+for i in range(1,T+1):
+    if i % 7 == 0 and i % 11 == 0:
+        print("Wiwat!")
+    elif i % 7 == 0:
+        print("Hurra!")
+    elif i % 11 == 0:
+        print("Super!")
+    else:
+        print(i)
