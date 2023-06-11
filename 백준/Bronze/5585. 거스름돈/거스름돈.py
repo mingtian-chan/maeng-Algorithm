25ea11a12c60101432b345d05a6d5755c34beb9b
Myoung Chan, Kim <89979849+mingtian-chan@users.noreply.github.com>
2023-06-11 22:44:49 +0900

    [Bronze II] Title: 거스름돈, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/5585. 거스름돈/거스름돈.py
+++ 백준/Bronze/5585. 거스름돈/거스름돈.py
@@ -1,20 +1,12 @@
-def greedy(i,cnt):
-    if i >= 500:
-        return greedy(i-500,cnt+1)
-    if i >= 100:
-        return greedy(i-100,cnt+1)
-    if i >= 50:
-        return greedy(i-50,cnt+1)
-    if i >= 10:
-        return greedy(i-10,cnt+1)
-    if i >= 5:
-        return greedy(i-5,cnt+1)
-    if i >= 1:
-        return greedy(i-1,cnt+1)
-    else:
-        return cnt
+def greedy(money,cnt):
+    lst = [500,100,50,10,5,1]
+
+    for i in lst:
+        cnt += (money//i)
+        money %= i
+    
+    return cnt
 
 
-
-i = 1000 - int(input())
-print(greedy(i,0))
+money = 1000 - int(input())
+print(greedy(money,0))
