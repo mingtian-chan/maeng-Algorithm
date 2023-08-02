cb490d72b9487ea686490547e90db00558240837
Myoung Chan, Kim <89979849+mingtian-chan@users.noreply.github.com>
2023-08-02 11:01:05 +0900

    [Bronze V] Title: 두 수 비교하기, Time: 212 ms, Memory: 17740 KB -BaekjoonHub

---
+++ 백준/Bronze/1330. 두 수 비교하기/두 수 비교하기.java
@@ -0,0 +1,23 @@
+//  두 수 비교하기
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        if (a > b) {
+            System.out.println(">");
+        }
+        if (a == b) {
+            System.out.println("==");
+        }
+        if (a < b) {
+            System.out.println("<");
+        }
+    }
+}
