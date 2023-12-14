3abfefb1e0b1dbe3d726f7e4c430fb8e26c181b1
Myoung Chan, Kim <89979849+mingtian-chan@users.noreply.github.com>
2023-12-14 17:00:54 +0900

    [Silver IV] Title: 카드2, Time: 160 ms, Memory: 123048 KB -BaekjoonHub

---
+++ 백준/Silver/2164. 카드2/카드2.py
@@ -0,0 +1,14 @@
+from collections import deque
+
+
+def test_problem_queue(num):
+    deq = deque([i for i in range(1, num + 1)])
+    while len(deq) > 1:
+        deq.popleft()
+        deq.append(deq.popleft())
+
+    return deq.popleft()
+
+i = int(input())
+print(test_problem_queue(i))
+
