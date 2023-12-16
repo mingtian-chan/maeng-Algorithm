103d4dfdc03ba94ad6f5a673fab08e3abbc2ffde
Myoung Chan, Kim <89979849+mingtian-chan@users.noreply.github.com>
2023-12-16 17:58:20 +0900

    [Silver I] Title: 단지번호붙이기, Time: 120 ms, Memory: 109240 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
@@ -1,41 +1,49 @@
 
 n = int(input())
 
+# 일단 adjMatrix를 만들고 시작하자.
 mat = [[-1 for i in range(n)] for j in range(n)]
 for i in range(n):
     s = input()
     for j in range(n):
-        mat[i][j] = int(s[j])
+        mat[j][i] = int(s[j])
 
 dx = [0, 0, 1, -1]
 dy = [1, -1, 0, 0]
 
-def dfs(x,y):
-    if x < 0 or x >= n or y < 0 or y >= n:
-        return False
+rows, cols = len(mat), len(mat[0])
 
-    if mat[y][x] == 1:
-        global cnt
-        cnt += 1
-        mat[y][x] = 0
-        for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
-            dfs(nx, ny)
-        return True
-    return False
-
-ans = []
 cnt = 0
-result = 0
+ans = []
+for row in range(rows):
+    for col in range(cols):
+        # 섬이 아닌 경우 건너 뜀.
+        if mat[row][col] != 1:
+            continue
 
-for i in range(n):
-    for j in range(n):
-        if dfs(j,i):
-            ans.append(cnt)
-            result += 1
-            cnt = 0
+        stack = [(row, col)]
+        cnt += 1
+        apart = 0
+        while stack:
+            x, y = stack.pop()
+            if mat[x][y] != 1:
+                continue
+            apart += 1
+            mat[x][y] = 0
+
+
+
+            for i in range(4):
+                nx = x + dx[i]
+                ny = y + dy[i]
+                if nx < 0 or nx >= rows or ny < 0 or ny >= cols:
+                    continue
+                stack.append((nx, ny))
+        ans.append(apart)
+
+print(cnt)
 ans.sort()
-print(result)
 for i in ans:
     print(i)
+
+
