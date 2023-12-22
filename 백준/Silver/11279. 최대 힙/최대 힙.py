d5f351ba8bda8b88ccc1648f44548cea80e86f7a
Myoung Chan, Kim <89979849+mingtian-chan@users.noreply.github.com>
2023-12-22 21:43:17 +0900

    [Silver II] Title: 최대 힙, Time: 204 ms, Memory: 114468 KB -BaekjoonHub

---
+++ 백준/Silver/11279. 최대 힙/최대 힙.py
@@ -0,0 +1,21 @@
+import heapq
+import sys
+
+heap = []
+
+n = int(sys.stdin.readline().rstrip())
+
+for _ in range(n):
+    i = -int(sys.stdin.readline().rstrip())
+    if i == 0:
+        if heap:
+            print(-heapq.heappop(heap))
+        else:
+            print(0)
+    else:
+        heapq.heappush(heap, i)
+
+
+
+
+
