600b3a9407dc149a8dd4eec749dc258ca1c29541
Myoung Chan, Kim <89979849+mingtian-chan@users.noreply.github.com>
2023-12-24 18:51:55 +0900

    [Gold V] Title: 트리, Time: 108 ms, Memory: 109240 KB -BaekjoonHub

---
+++ 백준/Gold/1068. 트리/트리.py
@@ -0,0 +1,20 @@
+def dfs(v):
+    tree[v] = -2
+    for i in range(n):
+        if v == tree[i]:
+            dfs(i)
+
+
+n = int(input())
+tree = list(map(int, input().split()))
+k = int(input())
+
+dfs(k)
+cnt = 0
+
+for i in range(n):
+    if tree[i] != -2 and i not in tree:
+        cnt += 1
+
+print(cnt)
+
