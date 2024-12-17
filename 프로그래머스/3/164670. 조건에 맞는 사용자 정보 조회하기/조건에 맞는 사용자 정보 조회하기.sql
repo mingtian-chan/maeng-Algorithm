e610eb033067a31da3bbadbc72dee8b7537aa1da
Myoung Chan, Kim <89979849+mingtian-chan@users.noreply.github.com>
2024-12-17 17:43:36 +0900

    [level 3] Title: 조건에 맞는 사용자 정보 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/3/164670. 조건에 맞는 사용자 정보 조회하기/조건에 맞는 사용자 정보 조회하기.sql
@@ -0,0 +1,18 @@
+-- 코드를 입력하세요
+SELECT
+    U.USER_ID,
+    U.NICKNAME,
+    CONCAT(CITY," ", STREET_ADDRESS1, " ", STREET_ADDRESS2) AS "전체주소",
+    CONCAT(
+        LEFT(TLNO,3),
+        "-",
+        SUBSTRING(TLNO, 4,4),
+        "-",
+        RIGHT(TLNO,4))
+                AS "전화번호"
+FROM USED_GOODS_BOARD B
+JOIN USED_GOODS_USER U
+ON B.WRITER_ID = U.USER_ID
+GROUP BY U.USER_ID
+HAVING COUNT(*) >= 3
+ORDER BY USER_ID DESC
